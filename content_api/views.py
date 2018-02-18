a55e0638865422a1ec87f407559b7ca024b39d30
Sunil <suryakantjp@gmail.com>
2018-02-18 16:39:27 +0530

    JSON API using DRF Viewsets

--- content_api/views.py
+++ content_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
+from .models import Item
+from .serializers import ItemSerializer
 # Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
