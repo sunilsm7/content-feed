721d91966f7b6841e1476dad05ac9415d109dd9e
Sunil <suryakantjp@gmail.com>
2018-02-18 17:18:10 +0530

    rendered list

--- contentfeed-fe/src/index.js
+++ contentfeed-fe/src/index.js
@@ -2,18 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ContentFeed extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      'items': []
+    }
+  }
   componentDidMount(){
     this.getItems();
   }
   getItems(){
     fetch('http://127.0.0.1:8000/api/item/')
       .then(results => results.json())
-      .then(({results}) => console.log(results));
+      .then(results => this.setState({
+        'items': results
+      }));
   }
 
   render() {
     return(
-      <h2>Content Feed</h2>
+      <div>
+        <h2>Content Feed</h2>
+        <ul>
+          {this.state.items.map((item, index) =>{
+            return (
+              <div key={item.id.toString()}>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
     );
   }
 }
